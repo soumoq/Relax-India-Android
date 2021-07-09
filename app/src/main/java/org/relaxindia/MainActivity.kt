63a1641c2628188359387669ae64721fcd1d5a2b
soumoq <soumojyotiganguly270@gmail.com>
2021-07-09 10:08:08 +0530

    Implement all dependency and complete MainActivity.kt

--- app/src/main/java/org/relaxindia/MainActivity.kt
+++ app/src/main/java/org/relaxindia/MainActivity.kt
@@ -1,11 +1,83 @@
 package org.relaxindia
 
+import android.Manifest
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatDelegate
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        val perms = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+
+        if (EasyPermissions.hasPermissions(this, *perms)) {
+            // we used the postDelayed(Runnable, time) method
+            // to send a message with a delayed time.
+            Handler().postDelayed({
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }, 3000) // 3000 is the delayed time in milliseconds.
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "This permission needed for access directory",
+                123,
+                *perms
+            )
+        }
+
     }
+
+    override fun onBackPressed() {
+        //super.onBackPressed()
+    }
+
+    override fun startActivity(intent: Intent?) {
+        super.startActivity(intent)
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: List<String?>) {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: List<String?>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == AppSettingsDialog.DEFAULT_SETTINGS_REQ_CODE) {
+        }
+    }
+
 }
