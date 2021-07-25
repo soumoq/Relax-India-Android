39fc566a570074be8d111f03105237f92e598140
soumoq <soumojyotiganguly270@gmail.com>
2021-07-26 01:09:07 +0530

    Complete track activity

--- app/src/main/java/org/relaxindia/view/activity/DriverFeedbackActivity.kt
+++ app/src/main/java/org/relaxindia/view/activity/DriverFeedbackActivity.kt
@@ -3,12 +3,19 @@ package org.relaxindia.view.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_driver_feedback.*
 import org.relaxindia.R
 
 class DriverFeedbackActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_driver_feedback)
+
+        feedback_submit.button.setOnClickListener {
+            val intent = Intent(this, ThankYouActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 
     override fun startActivity(intent: Intent?) {
---
+++ app/src/main/java/org/relaxindia/view/activity/ThankYouActivity.kt
@@ -0,0 +1,12 @@
+package org.relaxindia.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.relaxindia.R
+
+class ThankYouActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_thank_you)
+    }
+}
